ebc15d47c40233850249da0f143c93c35cc74447
Michael <mjs83111@uga.edu>
2024-05-18 10:44:42 -0400

    favicon1

--- server.js
+++ server.js
@@ -76,12 +76,11 @@ const { name, email, message, cc } = req.body;
     });
 });
 
-
+/*
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
-/*
+*/
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server listening at http://0.0.0.0:${port}`);
 });
-*/
